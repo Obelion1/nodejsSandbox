faa0c1bd64331b22471b6c8b7b83dc95f2c5acd4
Robin Quast <robin.quast@web.de>
2025-10-19 17:25:49 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hallo Welt</h1>');
+});
+ 
+server.listen(3000, '0.0.0.0', () => {
+    console.log('Server running at http://fi.mshome.net:3000/');
+});
+ 
+console.log('Mein Projekt wurde gestartet...')
