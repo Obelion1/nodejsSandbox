fe4fd71b07ecfedf04d24fcdcbef3bfd4115765e
Robin Quast <robin.quast@web.de>
2025-10-19 17:40:20 +0000

    added a quotes.txt and the functionality to read and display them on the website

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const http = require('http');
+const fs = require('fs');
+//reads txt, add each line as one element in the "lines" array
+const lines = fs.readFileSync('quotes.txt', 'utf8').split(/\r?\n/);
+console.log(lines);
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hallo Welt</h1>');
+  
+  // Generate HTML with all quotes, with for each
+  let html = '<h1>Famous Pop Culture Quotes</h1><ul>';
+  lines.forEach(quote => {
+    html += `<li>${quote}</li>`;
+  });
+  html += '</ul>';
+  
+  res.end(html);
 });
  
 server.listen(3000, '0.0.0.0', () => {
